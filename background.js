59521bc1d1aad859670c871317cd2ba21059797f
Denis Sokolov <denis@sokolov.cc>
2022-12-28 12:00:00 +0000

    manifest: upgrade to v3

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 var uuid = "9ee3ff5e_b744_4f08_ab24_355873ebfda5";
 
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
     if (!tab.url.match(/^(http|https|ftp|file)/)) {
         alert("Can't hide fixed elements in this type of page.");
         return;
     }
-    chrome.tabs.executeScript(tab.id, {
-        "code": "window." + uuid
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: (uuid) => window[uuid],
+        args: [uuid],
     }, function (r) {
         if (chrome.runtime.lastError) {
             alert("Unable to communicate with tab:\n" + chrome.runtime.lastError.message);
@@ -19,11 +22,12 @@ chrome.browserAction.onClicked.addListener(function (tab) {
             }
             chrome.tabs.sendMessage(tab.id, uuid + ":toggle");
         };
-        if (r[0]) {
+        if (r[0].result) {
             toggle();
         } else {
-            chrome.tabs.executeScript(tab.id, {
-                "file": "content.js"
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ["content.js"]
             }, toggle);
         }
     });
